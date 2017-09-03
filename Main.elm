46cbeaf5697e6c26fa122d0816b1d60c76856e57
ItamarGronich <itamar.gronich@fubndbox.com>
2017-09-03 10:18:33 +0300

    checkout buyiltin methods

--- Main.elm
+++ Main.elm
@@ -1,8 +1,39 @@
 module Main exposing (..)
 
-import Html exposing (h1, text)
+import Html exposing (h1, text, pre, div)
+
+
+model =
+    { foo = "foo"
+    , bar = "baz"
+    , num = 9
+    }
+
+
+headerText =
+    (++) "Hello" " World"
+
+
+headerTextModified : String
+headerTextModified =
+    let
+        delimiter : String
+        delimiter =
+            "! "
+    in
+        delimiter
+            |> String.append headerText
+            |> String.repeat 3
+            |> String.toUpper
+
+
+view model =
+    div []
+        [ text headerTextModified
+        , pre [] [ text (toString model) ]
+        ]
 
 
 main : Html.Html msg
 main =
-    h1 [] [ text "Hello World" ]
+    view model
